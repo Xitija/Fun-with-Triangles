80242d049b3d374d8329cc3e65519f17eefe2243
Kshitija Kadam <kshitijakadam04@gmail.com>
2022-09-24 23:38:54 +0530

    feat : highlighting right wrong questions in quiz

--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 const quizForm = document.querySelector(".quiz-form");
 const submitAnswerBtn = document.querySelector("#check-btn");
 const outputEl = document.querySelector("#output");
+const question = document.querySelectorAll(".question-container");
 
 const correctAnswers = ["90","right angled","perimeter","3","180","scalenet"];
 
@@ -11,6 +12,11 @@ function calculateScore(){
     for(let value of formResults.values()){
         if (value === correctAnswers[index]){
             score +=1 ;
+            question[index].style.backgroundColor = "#a1ff41";
+
+        }
+        else {
+            question[index].style.backgroundColor = "#fe4031";
         }
         index += 1;
     
